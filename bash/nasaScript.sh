077c17f54abf195bfa12d6bb903c945f567370de
barransapapa <barransapapa@gmail.com>
2025-11-22 00:36:19 -0600

    Avoid printing the API Key

--- bash/nasaScript.sh
+++ bash/nasaScript.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# Navigate to the directory of the script
 cd $(dirname "$0")
+# Prompt the user for their NASA API key
 echo -n "Enter your NASA API key to build the app environment: "
+# Save the input as a variable without echoing it to the terminal
 read -s apiKey
 echo
-echo "Your API key is $apiKey"
+# Apply Kubernetes configurations
 kubectl apply -f ../k8s/nasa_namespace.yaml
 kubectl create secret generic nasa-api-key --from-literal=apiKey=$apiKey -n nasa
 kubectl apply -f ../k8s/deployment.yaml
