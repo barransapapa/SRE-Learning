00e9bb8472b2f3cd6a3bda24abfd720e9dd63823
barransapapa <barransapapa@gmail.com>
2025-11-23 23:57:47 -0600

    App noiw runs under Alert Manager

--- python/app.py
+++ python/app.py
@@ -18,7 +18,7 @@ import logging
 from opentelemetry.exporter.otlp.proto.grpc._log_exporter import OTLPLogExporter
 from opentelemetry._logs import set_logger_provider
 from opentelemetry.sdk._logs import LoggerProvider, LoggingHandler
-from opentelemetry.sdk._logs.export import BatchLogProcessor
+from opentelemetry.sdk._logs.export import BatchLogRecordProcessor
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(filename='./logs/sre-app.log', encoding='utf-8', level=logging.DEBUG)
