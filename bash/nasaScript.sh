73b73caa3e02bd829e9f01b1bcff975e7d48c714
barransapapa <barransapapa@gmail.com>
2025-11-24 07:01:24 -0600

    Bash Script Change

--- bash/nasaScript.sh
+++ bash/nasaScript.sh
@@ -7,8 +7,7 @@ echo -n "Enter your NASA API key to build the app environment: "
 # Save the input as a variable without echoing it to the terminal
 read -s apiKey
 echo
-# Apply Kubernetes configurations
-#kubectl apply -f ../k8s/nasa_namespace.yaml
+# Apply Kubernetes configurations and deploy the application using Ansible
 ansible-playbook -i ../ansible/inventory.ini ../ansible/ansible_remove.yaml
 ansible-playbook -i ../ansible/inventory.ini ../ansible/ansible_nasa_namespace.yaml
 kubectl create secret generic nasa-api-key --from-literal=apiKey=$apiKey -n nasa
