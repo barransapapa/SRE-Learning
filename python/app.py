1dffa8e443ae03426083300bb754c30d9a9c1848
barransapapa <barransapapa@gmail.com>
2025-11-23 21:16:47 -0600

    Deployment REady

--- python/app.py
+++ python/app.py
@@ -12,6 +12,8 @@ from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.instrumentation.wsgi import OpenTelemetryMiddleware
 
+from prometheus_client import generate_latest, Counter, Histogram, Gauge
+
 import logging
 from opentelemetry.exporter.otlp.proto.grpc._log_exporter import OTLPLogExporter
 from opentelemetry._logs import set_logger_provider
@@ -26,7 +28,8 @@ logger.warning('This message should go to the log file with warning level')
 logger.error('This message should go to the log file with error level')
 
 # Set up OpenTelemetry tracing with my service name
-resource = Resource(attributes={"service.name": "nasa-app-service"})
+#resource = Resource(attributes={"service.name": "nasa-app-service"})
+resource = Resource.create({"service.name": "nasa-app-service"})
 
 span_exporter = OTLPSpanExporter(endpoint="otel-collector.opentelemetry.svc.cluster.local:4317", insecure=True)
 
@@ -53,6 +56,10 @@ app = Flask(__name__)
 FlaskInstrumentor().instrument_app(app)
 app.wsgi_app = OpenTelemetryMiddleware(app.wsgi_app)
 
+#Prometheus metrics
+REQUEST_COUNT = Counter('request_count', 'Total number of requests', ['method', 'endpoint'])
+REQUEST_LATENCY = Histogram('http_request_duration_seconds', 'HTTP Request Latency', ['method', 'endpoint'])
+
 @app.route('/') #These are the paths on the link
 def go_to_asteroids():
     return 'Please go to the /asteroids endpoint to see data about asteroids from NASA NeoWs API.'
@@ -66,14 +73,25 @@ def get_asteroids():
     #Retrieve the NASA API key from environment variables. The line below gets a secret from the environment to use as API key. You can get your own API key by registering at https://api.nasa.gov/.
     api_key = getenv("NASA_API_KEY")
     #Make a GET request to the NASA NeoWs API to retrieve asteroid data for the specified date
+    start_timer = time.time()
+    REQUEST_COUNT.labels(method='GET', endpoint='/asteroids').inc()
     url = f"https://api.nasa.gov/neo/rest/v1/feed?start_date={search_date}&end_date={search_date}&api_key={api_key}"
+    latency = time.time() - start_timer
+    REQUEST_LATENCY.labels(method='GET', endpoint='/asteroids').observe(latency)
     response = requests.get(url)
     data = response.json()
     with tracer.start_as_current_span("process_asteroid_data"):
         return {"data": data}
     
+@app.route('/metrics')
+def metrics():
+    return Response(generate_latest(), mimetype='text/plain')
 
 if __name__ == '__main__':
+    import os
+    logs_dir = './logs'
+    if not os.path.exists(logs_dir):
+        os.makedirs(logs_dir)
     app.run(host='0.0.0.0',port=5000)
 
 # Reference: https://apidog.com/es/blog/how-to-use-nasa-api-3/#servicio-web-de-objetos-cercanos-a-la-tierra-neows
